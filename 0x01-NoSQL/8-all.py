094e202e19499e24619c93d8702fc2c58b7a4b8b
mobashirr_wsl <zfzg223@gmail.com>
2024-11-26 01:52:41 +0300

    task 8

--- 0x01-NoSQL/8-all.py
+++ 0x01-NoSQL/8-all.py
@@ -1,4 +1,19 @@
 #!/usr/bin/env python3
+'''
+list all module
+'''
+
 
 def list_all(mongo_collection):
-    return mongo_collection.find()
+    '''
+    list all docs of collection
+
+    Args:
+        mongo_collection: The pymongo collection object.
+
+    Returns:
+        empty list if no document in the collection
+
+    '''
+    results = mongo_collection.find()
+    return results
