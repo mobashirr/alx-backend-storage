d2d674c01a2be3b8a60ddf7fd0ad02379f6eb3a6
mobashirr_wsl <zfzg223@gmail.com>
2024-11-26 01:46:30 +0300

    task 9

--- 0x01-NoSQL/9-insert_school.py
+++ 0x01-NoSQL/9-insert_school.py
@@ -1,1 +1,23 @@
 #!/usr/bin/python3
+
+'''
+insert based on **kwarg
+'''
+
+
+def insert_school(mongo_collection, **kwargs):
+    """
+    Inserts a new document into a MongoDB collection based on kwargs.
+    
+    Args:
+        mongo_collection: The pymongo collection object.
+        **kwargs: Key-value pairs representing the document to insert.
+        
+    Returns:
+        The _id of the newly inserted document.
+    """
+    if not kwargs:
+        raise ValueError("No data provided for insertion")
+    
+    result = mongo_collection.insert_one(kwargs)
+    return result.inserted_id
