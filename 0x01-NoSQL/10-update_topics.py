25ef9614e60c8caa08856cbc615a851362b6661d
mobashirr_wsl <zfzg223@gmail.com>
2024-11-26 02:07:23 +0300

    task 10

--- 0x01-NoSQL/10-update_topics.py
+++ 0x01-NoSQL/10-update_topics.py
@@ -1,1 +1,21 @@
 #!/usr/bin/env python3
+'''
+update topics
+'''
+
+def update_topics(mongo_collection, name, topics):
+    '''
+    update a document based on given name
+
+    Args:
+        mongo_collection: The pymongo collection object.
+        name: name which we want to change 
+        topics: (list of strings) will be the list of topics approached in the school
+
+    Returns: number of modyfied docs
+    '''
+    result = mongo_collection.update_many(
+        {'name':name},
+        {'$set':{'topics':topics}}
+    )
+    return result.modified_count
